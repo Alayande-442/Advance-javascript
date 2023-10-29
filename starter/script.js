da57dfc266f0ef3017f0b37134e04fb90ef8d2f4
Alayande olaoluwa Stephen <alayandesteven@gmail.com>
2023-10-29 10:35:27 +0100

    variable declaration

--- starter/script.js
+++ starter/script.js
@@ -6,3 +6,11 @@ this is my multi-line comment
 
 let myName = "olaoluwa";
 console.log(typeof(myName));
+
+let myNewName;
+console.log(myNewName)
+console.log(typeof(myNewName))
+
+let myNumber = null;
+console.log(typeof(myNumber));
+

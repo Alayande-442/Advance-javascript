a4f8205bef1b8f9aa01688dd22096f0dd4b85c09
Alayande olaoluwa Stephen <alayandesteven@gmail.com>
2023-11-08 13:56:46 +0100

    drills

--- starter2/drills.js
+++ starter2/drills.js
@@ -32,14 +32,46 @@ console.log(fruits);
 // }
 
 function displayName (names, age, hobbies) {
-    
     return `my name is ${names}, I'm ${age} years old, and I like playing ${hobbies}`;
-
-   
-
 }
-
-
 
 let mydisplay = displayName("olaoluwa", 20, "football");
 console.log(mydisplay);
+
+// An expression function
+let multiplicationTable = (a, b) => {
+    return a * b;
+
+}
+
+let result = multiplicationTable(100, 100)
+console.log(result);
+
+// function that calculate the area of a triangle
+let areaOfTriangle = (length, breath) => {
+    return (length * breath) / 2;
+}
+
+let myAreaCalculation = areaOfTriangle(10, 10)
+console.log(myAreaCalculation);
+
+// function that convert age to days
+
+let days = 365
+let calcAge = (age) => {
+    return `you have used ${age} years old on earth and is ${age * days} in days conversion`;
+
+}
+
+let myAgeCalculation = calcAge(20);
+console.log(myAgeCalculation);
+
+// function that joins another words
+
+let wordConcatenation = (fixWord) => {
+    return `${fixWord} comes from ${fixWord}`;
+
+}
+
+let concateResult = wordConcatenation("nothing");
+console.log(concateResult);

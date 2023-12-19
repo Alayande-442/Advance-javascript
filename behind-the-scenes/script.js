748a4e84120b746bcb00a6914f6263ffe24df919
Alayande-442 <alayandesteven@gmail.com>
2023-12-19 09:41:48 +0100

    how this keyworld works behind the scenes

--- behind-the-scenes/script.js
+++ behind-the-scenes/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+// COMMENT how this key word works
+
+const jonas = {
+  name: 'jonas',
+  year: '1990',
+
+  calAge: function () {
+    let cal = 2023 - this.year;
+    return cal;
+  },
+  greet: () => {
+    console.log(`hey ${this.name}`);
+  },
+};
+
+console.log(this.greet);
+let result = jonas.calAge();
+console.log(result);
+
+console.log(this);

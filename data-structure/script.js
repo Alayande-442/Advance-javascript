eafdcdb1d90f171e81e2a6fabf4bae0de73df509
Alayande-442 <alayandesteven@gmail.com>
2023-12-20 19:24:44 +0100

    data structure

--- data-structure/script.js
+++ data-structure/script.js
@@ -12,6 +12,15 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (startMenuIndex, mainMenuIndex) {
+    return [this.starterMenu[startMenuIndex], this.mainMenu[mainMenuIndex]];
+  },
+
+  pizzaDelivery: function (mainIngredient, ...otherIngredient) {
+    console.log(mainIngredient);
+    console.log(otherIngredient);
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -53,3 +62,143 @@ let temp = firstElement;
 firstElement = secondElement;
 secondElement = temp;
 console.log(firstElement, secondElement);
+
+[firstElement, secondElement] = [secondElement, firstElement];
+console.log(firstElement, secondElement);
+
+// working with object method
+console.log(restaurant.order(1, 2));
+
+let [, secondFood] = restaurant.order(1, 2);
+console.log(secondFood);
+
+// COMMENT Nested destructuring
+let arrp = [2, 4, [3, 5]];
+let [i, , [m, k]] = arrp;
+
+console.log(i, m, k);
+
+let newArrp = [].concat(arrp);
+console.log(newArrp);
+console.log(arrp[2][0]);
+
+// COMMENT How to destructure an object
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// const {
+//   menu: [],
+//   startMenuIndex: starter = [],
+// } = restaurant;
+
+// console.log(menu, starterMenuIndex);
+
+// COMMENT spread ...operators
+
+const numberArr = [6, 7, 8, 9];
+
+const newArr = [1, 2, 3, 4, 5, ...numberArr];
+console.log(newArr);
+
+const addedMenu = [...restaurant.mainMenu, 'chickwiz', 'bouger'];
+console.log(addedMenu);
+
+const allMenu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(allMenu);
+
+// COMMENT simple drill
+const studentName2 = ['ola', 'john', 'peter', 'mathew'];
+const [, , , mathew] = studentName2;
+console.log(mathew);
+
+const objectdest = {
+  name1: 'ola',
+  age: '200',
+  address: 'washing street',
+};
+
+const { name1, age, address } = objectdest;
+console.log(name1, age, address);
+
+// COMMENT spread operator drill
+const fruits = ['cashew', 'mango', 'kola'];
+
+const newFruit = ['pineapple', 'melon', ...fruits];
+console.log(newFruit);
+
+// COMMENT spread operators on object
+const restaurantCopy = { ...restaurant };
+console.log(restaurantCopy);
+
+restaurantCopy.name = 'olaoluwa';
+console.log(restaurantCopy);
+console.log(restaurant);
+
+const [x, y, ...others] = [1, 2, 3, 4, 5, 6];
+console.log(x, y, others);
+
+// COMMENT rest pattern in object
+
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(sat, weekdays);
+
+// COMMENT using pattern with function
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+
+  console.log(numbers);
+  console.log(sum);
+};
+
+add(1, 2);
+add(3, 4, 5, 6);
+add(7, 8, 8, 10);
+
+restaurant.pizzaDelivery('dodo', 'rice', 'beans', 'spaghetti');
+
+// COMMENT short circuiting && and ||
+
+console.log('----OR short circuiting -----');
+console.log(3 || 'jonas');
+
+console.log('' || 'ola');
+console.log(undefined || null);
+
+console.log('----AND short circuiting -----');
+
+console.log(0 && 'ola');
+
+// COMMENT For - of - loop COMMENT
+
+for (const item of restaurant.mainMenu) console.log(item);
+
+let myFruits = ['apple', 'mango', 'guava'];
+
+for (let items of myFruits) console.log(items);
+for (let items of myFruits.entries()) console.log(items);
+
+const days = ['mon', 'tues', 'wed', 'thur', 'fri', 'sat', 'sun'];
+for (let items of days) console.log(items);
+
+// COMMENT using optional chaining with an arrays
+const myInfo = [{ name: 'olaoluwa', email: 'alayandesteven@gmail.com' }];
+
+console.log(myInfo[0].name ?? 'user array is empty');
+console.log(myInfo[0].email ?? 'user array is empty');
+
+// COMMENT looping through an object
+
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+console.log(`we open on ${properties.length} days`);
